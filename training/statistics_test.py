3c4a703263e73ed063c631320e45eb80890ee789
Narzhan <vasek.bruzek@gmail.com>
2018-11-17 09:44:46 +0100

    Solved SVC and feature importances

--- training/statistics_test.py
+++ training/statistics_test.py
@@ -1,10 +1,11 @@
 import pandas
+import time
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 from xgboost import XGBClassifier
 from catboost import CatBoostClassifier
 from sklearn import model_selection
-import lightgbm as lgb
+# import lightgbm as lgb
 from sklearn.metrics import mean_squared_error
 from sklearn import decomposition
 from sklearn.metrics import classification_report
@@ -23,6 +24,7 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.svm import SVC
+from sklearn.svm import NuSVC
 from sklearn.decomposition import FastICA
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.feature_selection import SelectFromModel
@@ -30,9 +32,11 @@ from sklearn.feature_selection import chi2
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import RFE
 from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVR
 from sklearn.feature_selection import f_regression
+from sklearn.ensemble import BaggingClassifier
 
-# dataset = pandas.read_csv("result.csv")
+#dataset = pandas.read_csv("result.csv")
 dataset = pandas.read_csv("test_data.csv", index_col=13)
 print(dataset.shape)
 print(round(dataset.describe(),2))
@@ -48,7 +52,7 @@ print(dataset.groupby("label").size())
 array = dataset.values
 X = array[:, 0:12]
 Y = array[:, 12]
-validation_size = 0.20
+validation_size = 0.80
 seed = 7
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
@@ -57,8 +61,6 @@ scoring = 'accuracy'
 
 print("Variances: {}".format(dataset.var()))
 print("Correlations: {}".format(dataset.corr()))
-# rf_exp = RandomForestRegressor(n_estimators=1000, random_state=100)
-# rf_exp.fit(X_train, Y_train)
 rf_exp = ExtraTreesClassifier(n_estimators=50)
 rf_exp = rf_exp.fit(X_train, Y_train)
 importances = list(rf_exp.feature_importances_)
@@ -66,6 +68,20 @@ feature_importances = [(feature, round(importance, 2)) for feature, importance i
 feature_importances = sorted(feature_importances, key=lambda x: x[1], reverse=True)
 [print('Variable(Extra treee classifier) : {:20} Importance: {}'.format(*pair)) for pair in feature_importances]
 
+rf_exp = DecisionTreeClassifier()
+rf_exp = rf_exp.fit(X_train, Y_train)
+importances = list(rf_exp.feature_importances_)
+feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(list(dataset), importances)]
+feature_importances = sorted(feature_importances, key=lambda x: x[1], reverse=True)
+[print('Variable(decision treee classifier) : {:20} Importance: {}'.format(*pair)) for pair in feature_importances]
+
+
+rf_exp = GradientBoostingClassifier()
+rf_exp = rf_exp.fit(X_train, Y_train)
+importances = list(rf_exp.feature_importances_)
+feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(list(dataset), importances)]
+feature_importances = sorted(feature_importances, key=lambda x: x[1], reverse=True)
+[print('Variable(GradientBoostingClassifier) : {:20} Importance: {}'.format(*pair)) for pair in feature_importances]
 
 # rf_exp = RandomForestRegressor(n_estimators=1000, random_state=100)
 # rf_exp.fit(X_train, Y_train)
@@ -78,7 +94,10 @@ model = SelectFromModel(rf_exp, prefit=True)
 X_new = model.transform(X_train)
 print("Select from model shape: {}".format(X_new.shape))
 
-X_new = SelectKBest(chi2, k=5).fit_transform(X_train, Y_train)
+model = SelectKBest(chi2, k=5)
+fit = model.fit(X_train, Y_train)
+print("Select k best priorities: {}".format(fit.scores_))
+X_new=fit.transform(X_train)
 print("Select k best with chi2: {}".format(X_new.shape))
 
 lsvc = LinearSVC(C=0.01, penalty="l1", dual=False,max_iter=2000).fit(X, Y)
@@ -92,27 +111,36 @@ selector = RFE(estimator, 5)
 selector = selector.fit(X, Y)
 print("RFE count features: {}, features: {}, ranking: {}".format(selector.n_features_, selector.support_, selector.ranking_))
 
-# implement forward/backward ? feature selection
+pca = decomposition.PCA(n_components=12)
+fit = pca.fit(X_train, Y_train)
+X_new = pca.transform(X_train)
+print("PCA: {}".format(X_new.shape))
+# print("PCA components: {}".format(fit.components_))
+# print("PCA variance: {}".format(fit.explained_variance_ratio_))
+
+ica=FastICA(n_components=12, random_state=0)
+x_reduced=ica.fit_transform(X_train)
+print("ICA: {}".format(x_reduced.shape))
+
 
 models = []
-models.append(('LR', LogisticRegression(solver='lbfgs', class_weight="balanced"))) #, n_jobs=-1, warm_start=True
-# models.append(('LR(sag, no balance)', LogisticRegression(multi_class="ovr", solver='sag', n_jobs=-1, warm_start=True)))
-# models.append(('LR(newton-cg, no balance)', LogisticRegression(multi_class="ovr", solver='newton-cg', n_jobs=-1, warm_start=True, max_iter=400)))
+# from sklearn.preprocessing import MinMaxScaler
+# scaling = MinMaxScaler(feature_range=(-1,1)).fit(X_train)
+# X_train = scaling.transform(X_train)
+# X_validation = scaling.transform(X_validation)
+
+models.append(('LR', LogisticRegression(solver='lbfgs', class_weight="balanced")))
 models.append(('LDA', LinearDiscriminantAnalysis()))
-# models.append(('LDA(lsqr)', LinearDiscriminantAnalysis(solver="lsqr", shrinkage="auto")))
-# models.append(('LDA(eigen)', LinearDiscriminantAnalysis(solver="eigen", shrinkage="auto")))
-models.append(('KNN', KNeighborsClassifier())) # weights="distance", algorithm="auto"
-# models.append(('KNN(uniform)', KNeighborsClassifier(weights="uniform", algorithm="auto")))
+models.append(('KNN', KNeighborsClassifier()))
 models.append(('DecTree', DecisionTreeClassifier())) # gini, best
-# models.append(('DesitionTree(entropy)', DecisionTreeClassifier(criterion="entropy", splitter="random")))
 models.append(('NB', GaussianNB()))
 models.append(('MNB', MultinomialNB()))
 models.append(('RForest', RandomForestClassifier(n_estimators=100)))
-# models.append(('RForest', RandomForestClassifier(n_estimators=100, criterion="entropy", warm_start=True)))
 models.append(('Kmeans', KMeans()))
 models.append(('Ada', AdaBoostClassifier()))
-# models.append(('SVM', SGDClassifier()))
-models.append(('SVM', SVC(gamma="scale")))
+models.append(('GaussianNB', GaussianNB()))
+#models.append(('SVM(linear))', LinearSVC(max_iter=4000)))
+#models.append(('SVM', SVC(gamma="scale", cache_size=2000.0)))
 models.append(('GBC', GradientBoostingClassifier()))
 models.append(('SQD', SGDClassifier()))
 models.append(('XGB', XGBClassifier()))
@@ -120,6 +148,7 @@ models.append(('CatBoost', CatBoostClassifier(iterations=2, learning_rate=1, dep
 
 results = []
 names = []
+start_time = time.time()
 for name, model in models:
     try:
         kfold = model_selection.KFold(n_splits=10, random_state=seed)
@@ -131,6 +160,7 @@ for name, model in models:
         print(e)
     else:
         print(msg)
+print("Took: {}".format(time.time() - start_time))
 
 # # train_data = lgb.Dataset(X_train, label=Y_train)
 # lgb_train = lgb.Dataset(X_train, Y_train)
@@ -152,7 +182,7 @@ for name, model in models:
 # y_pred = bst.predict(X_validation, num_iteration=bst.best_iteration)
 # print('The mean of prediction is:', y_pred.mean(), y_pred.std())
 
-# Make predictions on validation dataset
+
 knn = KNeighborsClassifier()
 knn.fit(X_train, Y_train)
 predictions = knn.predict(X_validation)
@@ -160,31 +190,16 @@ print(accuracy_score(Y_validation, predictions))
 print(confusion_matrix(Y_validation, predictions))
 print(classification_report(Y_validation, predictions))
 
-# Factor reduction, Create PCA object
-# k =min(n_sample, n_features)
-pca= decomposition.PCA(n_components=12)
-# For Factor analysis
-fa= decomposition.FactorAnalysis()
-# Reduced the dimension of training dataset using PCA
-train_reduced = pca.fit_transform(X_train)
-#Reduced the dimension of test dataset
-test_reduced = pca.transform(X_validation)
-print("PCA: {}".format(test_reduced.shape))
-print("PCA componeents: {}".format(test_reduced.components_))
 
-#Ica
-ica=FastICA(n_components=12, random_state=0)
-x_reduced=ica.fit_transform(X_train)
-print("ICA: {}".format(x_reduced.shape))
-
-# # Compare Algorithms
-# fig = plt.figure(figsize=(10.0, 8.0))
-# fig.suptitle('První test - porovnání algortimů')
-# ax = fig.add_subplot(111)
-# plt.boxplot(results, 0, '')
-# ax.set_xticklabels(names)
-# plt.ylabel("Přesnost")
-# for tick in ax.get_xticklabels():
-#     tick.set_rotation(70)
-# plt.savefig("data.png")
+fig = plt.figure(figsize=(10.0, 8.0))
+ax = fig.add_subplot(111)
+plt.boxplot(results, 0, '')
+ax.set_xticklabels(names)
+plt.ylabel("Přesnost")
+for tick in ax.get_xticklabels():
+    tick.set_rotation(70)
+plt.savefig("test_data.png")
+# with open("result.txt", "w") as file:
+# 	for name, result in zip(names, results):
+# 		file.write("{},{}\n".format(name, result))
 # plt.show()
