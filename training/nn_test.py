769ef34e3b119905ce85e4189afc7049b17eb938
Narzhan <vasek.bruzek@gmail.com>
2019-01-11 09:19:41 +0100

    Reorder of nn test

--- training/nn_test.py
+++ training/nn_test.py
@@ -24,27 +24,22 @@ def load_data(cols: list):
     test_dataset = pd.read_csv("text_test_data.csv", index_col=2, encoding='utf-8', delimiter=";", engine="python")
     test_dataset = test_dataset.replace(np.nan, '', regex=True)
     test_dataset = test_dataset.sort_index()
-    # tfidf = TfidfVectorizer(min_df=0.2, analyzer='word', stop_words="english")
-    # features = tfidf.fit_transform(test_dataset.text)
     tfidf = pickle.load(open("binaries/tfidf(0.1).pkl", "rb"))
     features = tfidf.transform(test_dataset.text)
     test_dataset, tfidf = None, None
     topics = pickle.load(open("gensim_lda/topics_25.pkl", "rb"))
+    w2v_labels = pickle.load(open("cluster_labels.pkl", "rb")).reshape((-1, 1))
 
     array = dataset.values
     X = array[:, 0:len(cols) - 2]
     Y = array[:, len(cols) - 2]
-    X = sparse.hstack([features, topics, X])
+    X = sparse.hstack([features, topics, w2v_labels, X])
 
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
     return X_train, X_test, y_train, y_test, X.shape[1]
 
 
-def lear_nn(cols: list):
-    # sc = StandardScaler()
-    # X_train = sc.fit_transform(X_train)
-    # X_test = sc.transform(X_test)
-    X_train, X_test, y_train, y_test, length = load_data(cols)
+def baseline_nn():
     start_time = time.time()
     classifier = Sequential()
     # Adding the input layer and the first hidden layer
@@ -58,8 +53,11 @@ def lear_nn(cols: list):
 
     # Fitting our model
     classifier.fit(X_train, y_train, batch_size=100, nb_epoch=10)
-    print(cols)
     print("result: {}".format(time.time() - start_time))
+    evaluate_model(classifier)
+
+
+def evaluate_model(classifier):
     y_pred = classifier.predict(X_test)
     y_pred = (y_pred > 0.5)
     cm = confusion_matrix(y_test, y_pred)
@@ -69,12 +67,8 @@ def lear_nn(cols: list):
     print(diagonal_sum / sum_of_all_elements)
 
 
-if __name__ == '__main__':
-    columns = ['full_path', 'part_path', 'about',
+columns = ['full_path', 'part_path', 'about',
                'deep_links', 'fresh', 'infection', 'pages', 'totalEstimatedMatches', 'someResultsRemoved', 'label',
                "domain"]
-    # for column in ['full_path', 'about', 'deep_links', 'fresh', 'infection', 'someResultsRemoved']:
-    #     columns.remove(column)
-    now = time.time()
-    lear_nn(columns)
-    print(time.time() - now)
+X_train, X_test, y_train, y_test, length = load_data(columns)
+baseline_nn()
