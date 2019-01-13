ea5aba10b8935255e331da7d359a4d4e68b5dfa4
Narzhan <vasek.bruzek@gmail.com>
2019-01-13 09:25:48 +0100

    Added test options for w2v analysis with lstm

--- training/test_lstm.py
+++ training/test_lstm.py
@@ -1,5 +1,7 @@
+from keras import Model, Input
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation
+from keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation, SpatialDropout1D, GRU, \
+    Bidirectional
 from keras.layers.embeddings import Embedding
 import pickle, gensim
 from gensim.models import word2vec
@@ -12,7 +14,6 @@ def sentence_vectorizer(words, model, vocabulary, num_features):
     for word in words:
         if word in vocabulary:
             feature_vector = np.add(feature_vector, model[word])
-        print(feature_vector)
     return feature_vector
 
 
@@ -35,14 +36,92 @@ window_context = 20  # Context window size
 w2v_model = word2vec.Word2Vec.load("gensim_we/w2v_model_splitted.pkl")
 # X = w2v_model[w2v_model.wv.vocab]
 w2v_feature_array = corpus_vectorizer(corpus=processed_docs, model=w2v_model, num_features=feature_size)
-w2v_model, processed_docs = None, None
+processed_docs = None
+pickle.dump(w2v_feature_array, open("w2v_feature_array_splitted.pkl", "wb"))
 dataset = pandas.read_csv("text_test_data_splitted.csv", index_col=3, encoding='utf-8', delimiter=";", engine="python")
 dataset = dataset.replace(np.nan, '', regex=True)
 dataset = dataset.sort_index()
 
-model_lstm = Sequential()
-model_lstm.add(Embedding(len(w2v_feature_array), feature_size, input_length=feature_size, trainable=False))
-model_lstm.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
-model_lstm.add(Dense(1, activation='sigmoid'))
-model_lstm.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model_lstm.fit(w2v_feature_array, dataset.label, validation_split=0.2, epochs=10)
+
+# model_lstm = Sequential()
+# model_lstm.add(Embedding(len(w2v_feature_array), feature_size, input_length=feature_size, trainable=False))
+# model_lstm.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+# model_lstm.add(Dense(1, activation='sigmoid'))
+# model_lstm.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# model_lstm.fit(w2v_feature_array, dataset.label, validation_split=0.2, epochs=10)
+
+
+def create_rnn_lstm():
+    # Add an Input Layer
+    input_layer = Input((70,))
+
+    # Add the word embedding Layer
+    embedding_layer = Embedding(input_dim=w2v_model.syn0.shape[0], output_dim=w2v_model.syn0.shape[1],
+                                weights=[w2v_model.syn0])(
+        input_layer)
+    embedding_layer = SpatialDropout1D(0.3)(embedding_layer)
+
+    # Add the LSTM Layer
+    lstm_layer = LSTM(100)(embedding_layer)
+
+    # Add the output Layers
+    output_layer1 = Dense(50, activation="relu")(lstm_layer)
+    output_layer1 = Dropout(0.25)(output_layer1)
+    output_layer2 = Dense(1, activation="sigmoid")(output_layer1)
+
+    # Compile the model
+    model = Model(inputs=input_layer, outputs=output_layer2)
+    model.compile(optimizer="adam", loss='binary_crossentropy', metrics=['accuracy'])
+
+    # Fitting our model
+    # model.fit(X_train, y_train, batch_size=100, nb_epoch=10)
+
+create_rnn_lstm()
+
+def create_rnn_gru():
+    # Add an Input Layer
+    input_layer = Input((70,))
+
+    # Add the word embedding Layer
+    embedding_layer = Embedding(len(w2v_feature_array) + 1, 300, weights=[embedding_matrix], trainable=False)(
+        input_layer)
+    embedding_layer = SpatialDropout1D(0.3)(embedding_layer)
+
+    # Add the GRU Layer
+    lstm_layer = GRU(100)(embedding_layer)
+
+    # Add the output Layers
+    output_layer1 = Dense(50, activation="relu")(lstm_layer)
+    output_layer1 = Dropout(0.25)(output_layer1)
+    output_layer2 = Dense(1, activation="sigmoid")(output_layer1)
+
+    # Compile the model
+    model = Model(inputs=input_layer, outputs=output_layer2)
+    model.compile(optimizer="adam", loss='binary_crossentropy', metrics=['accuracy'])
+
+    # Fitting our model
+    model.fit(X_train, y_train, batch_size=100, epochs=10)
+
+def create_bidirectional_rnn():
+    # Add an Input Layer
+    input_layer = Input((70,))
+
+    # Add the word embedding Layer
+    embedding_layer = Embedding(len(w2v_feature_array) + 1, 300, weights=[embedding_matrix], trainable=False)(
+        input_layer)
+    embedding_layer = SpatialDropout1D(0.3)(embedding_layer)
+
+    # Add the LSTM Layer
+    lstm_layer = Bidirectional(GRU(100))(embedding_layer)
+
+    # Add the output Layers
+    output_layer1 = Dense(50, activation="relu")(lstm_layer)
+    output_layer1 = Dropout(0.25)(output_layer1)
+    output_layer2 = Dense(1, activation="sigmoid")(output_layer1)
+
+    # Compile the model
+    model = Model(inputs=input_layer, outputs=output_layer2)
+    model.compile(optimizer="adam", loss='binary_crossentropy', metrics=['accuracy'])
+
+    # Fitting our model
+    model.fit(X_train, y_train, batch_size=100, nb_epoch=10)
