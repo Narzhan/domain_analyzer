dcd4699a71fa721f04b243de1cb46868fcf2accf
Narzhan <vasek.bruzek@gmail.com>
2019-02-09 09:39:31 +0100

    Updates to word embedding file

--- training/word_embedding.py
+++ training/word_embedding.py
@@ -2,11 +2,16 @@ import pandas, nltk
 import numpy as np
 import string
 from gensim.models import word2vec
+import matplotlib.pyplot as plt
 from gensim.models import FastText
 import gensim, pickle
 from keras import Input, Model
 from keras.layers import Embedding, SpatialDropout1D, LSTM, Dense, Dropout, GRU, Bidirectional
 from keras.preprocessing.text import Tokenizer
+from numpy import asarray
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.sequence import pad_sequences
 from nltk import SnowballStemmer, WordNetLemmatizer
 #
 #
@@ -69,82 +74,110 @@ from nltk import SnowballStemmer, WordNetLemmatizer
 # cluster_labels = pandas.DataFrame(cluster_labels, columns=['ClusterLabel'])
 # print(pandas.concat([dataset, cluster_labels], axis=1))
 
-from numpy import asarray
-from sklearn.metrics import confusion_matrix
 
 dataset = pandas.read_csv("text_test_data_splitted.csv", index_col=3, encoding='utf-8', delimiter=";", engine="python")
 dataset = dataset.replace(np.nan, '', regex=True)
 dataset = dataset.sort_index()
 languages = ["en", "cs", "de", "es", "fr", "ja", "ru", "zh"]
-filter_languages = ["en", "de", "es", "fr", "ja", "ru", "zh"]
+# filter_languages = ["en", "de", "es", "fr", "ja", "ru", "zh"]
 # pretrained_dataset = dataset.loc[dataset["language"].isin(languages)]
 customtrain_dataset = dataset.loc[~dataset["language"].isin(languages)]
-dataset = dataset.loc[~dataset["language"].isin(filter_languages)]
 
 
 def preprocess(text):
     # convert to list the input
-    result = []
-    for line in text:
-        tokens = [key.lower() for key in nltk.word_tokenize(line)]
-        table = str.maketrans("", "", string.punctuation)
-        stripped = [key.translate(table) for key in tokens]
-        words = [word for word in stripped if word.isalpha()]
-        result.append(words)
-    return result
+    tokens = [key.lower() for key in nltk.word_tokenize(text)]
+    words = [word for word in tokens if word.isalpha()]
+    return words
 
+
+# seq_lengths = dataset.text.apply(lambda x: len(x.split(' ')))
+# print(seq_lengths.describe())
 
 processed_docs = customtrain_dataset['text'].map(preprocess)
+# processed_docs = pickle.load(open("splitted_docs.pkl", "rb"))
+# pickle.dump(processed_docs, open("splitted_text/word_embedding/splitted_docs.pkl", "wb"))
 feature_size = 300
 window_context = 10
 min_word_count = 5
 sample = 1e-3
 
+
+####### Fast Text
 fast_text = FastText(processed_docs, size=feature_size,
                      window=window_context, min_count=min_word_count, sample=sample,
                      iter=50)
 fast_text.save("splitted_text/word_embedding/fast_text/fast_text.pkl")
 fast_text.wv.save_word2vec_format("pretrained/custom_embedding.txt", binary=False)
+fast_text, processed_docs = None, None
+
+######Word2Vec
+# w2v_model = word2vec.Word2Vec(processed_docs, size=feature_size,
+#                               window=window_context, min_count=min_word_count,
+#                               sample=sample, iter=10)
+# w2v_model.save("w2v_model.pkl")
 
 
 t = Tokenizer()
 t.fit_on_texts(dataset.text)
 vocab_size = len(t.word_index) + 1
 embedding_matrix = np.zeros((vocab_size, 300))
-# files_location = ["wiki.cs.vec", "wiki.de.vec", "wiki.en.vec", "wiki.es.vec", "wiki.fr.vec", "wiki.ja.vec",
-#                   "wiki.ru.vec", "wiki.zh.vec"]
-files_location = ["wiki.cs.vec"]
+files_location = ["wiki.cs.vec", "wiki.de.vec", "wiki.en.vec", "wiki.es.vec", "wiki.fr.vec", "wiki.ja.vec",
+                  "wiki.ru.vec", "wiki.zh.vec"]
+# files_location = ["wiki.cs.vec"]
 for file_location in files_location:
-    with open("pretrained/{}".format(files_location), "r") as file:
+    with open("pretrained/{}".format(file_location), "r") as file:
         for line in file:
             values = line.split()
             if values[0] in t.word_index:
-                word = values[0]
+                # word = values[0]
                 coefs = asarray(values[len(values)-300:], dtype='float32')
-                embedding_matrix[t.word_index[word]] = coefs
+                embedding_matrix[t.word_index[values[0]]] = coefs
 
 with open("pretrained/custom_embedding.txt", "r") as file:
     for line in file:
         values = line.split()
-        if values[0] in t.word_index:
-            word = values[0]
-            coefs = asarray(values[len(values) - 300:], dtype='float32')
-            embedding_matrix[t.word_index[word]] = coefs
+        # if values[0] in t.word_index:
+        # word = values[0]
+        coefs = asarray(values[len(values) - 300:], dtype='float32')
+        embedding_matrix[t.word_index[values[0]]] = coefs
 np.save("splitted_text/word_embedding/fast_text/embedding_martix.npy", embedding_matrix)
 
-X_train, y_train, X_validation, y_validation= None, None, None,None
+####Wod2vec
+# embedding_matrix = np.zeros((len(w2v_model.wv.vocab), feature_size))
+# for i in range(len(w2v_model.wv.vocab)):
+#     embedding_matrix[t.word_index[values[0]]] = coefs
+#
+#     embedding_vector = w2v_model.wv[w2v_model.wv.index2word[i]]
+#     if embedding_vector is not None:
+#         embedding_matrix[i] = embedding_vector
+
+
+# X_train, X_test, y_train, y_test = train_test_split(dataset.text, dataset.label, test_size=0.1, random_state=7)
+# X_train_seq = t.texts_to_sequences(X_train)
+# X_test_seq = t.texts_to_sequences(X_test)
+MAX_LEN = 134
+X_train_seq_trunc = pad_sequences(t.texts_to_sequences(dataset.text), maxlen=MAX_LEN)
+# X_test_seq_trunc = pad_sequences(t.texts_to_sequences(X_test), maxlen=MAX_LEN)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train_seq_trunc, dataset.label, test_size=0.2,
+                                                      random_state=7)
+X_train_seq_trunc = None
+dataset, t = None, None
+num_epoches = 15
+batch = 64
+num_neurons = 100
 
 def create_rnn_lstm():
     # Add an Input Layer
-    input_layer = Input((70,))
+    input_layer = Input((MAX_LEN,))
 
     # Add the word embedding Layer
-    embedding_layer = Embedding(vocab_size, 300, weights=[embedding_matrix], trainable=False)(
+    embedding_layer = Embedding(vocab_size, feature_size, weights=[embedding_matrix], trainable=False)(
         input_layer)
     embedding_layer = SpatialDropout1D(0.3)(embedding_layer)
 
     # Add the LSTM Layer
-    lstm_layer = LSTM(100)(embedding_layer)
+    lstm_layer = LSTM(num_neurons)(embedding_layer)
 
     # Add the output Layers
     output_layer1 = Dense(50, activation="relu")(lstm_layer)
@@ -156,21 +189,25 @@ def create_rnn_lstm():
     model.compile(optimizer="adam", loss='binary_crossentropy', metrics=['accuracy'])
 
     # Fitting our model
-    model.fit(X_train, y_train, batch_size=100, nb_epoch=10)
+    history = model.fit(X_train, y_train, batch_size=batch, nb_epoch=num_epoches)
     evaluate_model(model)
+    eval_metric(history, "acc", "en")
+    eval_metric(history, "loss", "en")
+    eval_metric(history, "acc", "cz")
+    eval_metric(history, "loss", "cz")
 
 
 def create_rnn_gru():
     # Add an Input Layer
-    input_layer = Input((70,))
+    input_layer = Input((MAX_LEN,))
 
     # Add the word embedding Layer
-    embedding_layer = Embedding(vocab_size, 300, weights=[embedding_matrix], trainable=False)(
+    embedding_layer = Embedding(vocab_size, feature_size, weights=[embedding_matrix], trainable=False)(
         input_layer)
     embedding_layer = SpatialDropout1D(0.3)(embedding_layer)
 
     # Add the GRU Layer
-    lstm_layer = GRU(100)(embedding_layer)
+    lstm_layer = GRU(num_neurons)(embedding_layer)
 
     # Add the output Layers
     output_layer1 = Dense(50, activation="relu")(lstm_layer)
@@ -182,21 +219,25 @@ def create_rnn_gru():
     model.compile(optimizer="adam", loss='binary_crossentropy', metrics=['accuracy'])
 
     # Fitting our model
-    model.fit(X_train, y_train, batch_size=100, epochs=10)
+    history = model.fit(X_train, y_train, batch_size=batch, epochs=num_epoches)
     evaluate_model(model)
+    eval_metric(history, "acc", "en")
+    eval_metric(history, "loss", "en")
+    eval_metric(history, "acc", "cz")
+    eval_metric(history, "loss", "cz")
 
 
-def create_bidirectional_rnn(X_train, y_train):
+def create_bidirectional_rnn():
     # Add an Input Layer
-    input_layer = Input((70,))
+    input_layer = Input((MAX_LEN,))
 
     # Add the word embedding Layer
-    embedding_layer = Embedding(vocab_size, 300, weights=[embedding_matrix], trainable=False)(
+    embedding_layer = Embedding(vocab_size, feature_size, weights=[embedding_matrix], trainable=False)(
         input_layer)
     embedding_layer = SpatialDropout1D(0.3)(embedding_layer)
 
     # Add the LSTM Layer
-    lstm_layer = Bidirectional(GRU(100))(embedding_layer)
+    lstm_layer = Bidirectional(GRU(num_neurons))(embedding_layer)
 
     # Add the output Layers
     output_layer1 = Dense(50, activation="relu")(lstm_layer)
@@ -208,15 +249,47 @@ def create_bidirectional_rnn(X_train, y_train):
     model.compile(optimizer="adam", loss='binary_crossentropy', metrics=['accuracy'])
 
     # Fitting our model
-    model.fit(X_train, y_train, batch_size=100, nb_epoch=10)
+    history = model.fit(X_train, y_train, batch_size=batch, nb_epoch=num_epoches)
     evaluate_model(model)
+    eval_metric(history, "acc", "en")
+    eval_metric(history, "loss", "en")
+    eval_metric(history, "acc", "cz")
+    eval_metric(history, "loss", "cz")
+
+
+def eval_metric(history, metric_name, lang):
+    '''
+    Function to evaluate a trained model on a chosen metric.
+    Training and validation metric are plotted in a
+    line chart for each epoch.
+
+    Parameters:
+        history : model training history
+        metric_name : loss or accuracy
+    Output:
+        line chart with epochs of x-axis and metric on
+        y-axisbatch
+    '''
+    metric = history.history[metric_name]
+    val_metric = history.history['val_' + metric_name]
+
+    e = range(1, num_epoches + 1)
+
+    if lang == "cz":
+        train, valid = "Trénovací ", "Validační "
+    else:
+        train, valid = "Train ", "Validation "
+    plt.plot(e, metric, 'bo', label=train + metric_name)
+    plt.plot(e, val_metric, 'b', label=valid + metric_name)
+    plt.legend()
+    plt.show()
 
 
 def evaluate_model(classifier):
-    y_pred = classifier.predict(X_validation)
+    y_pred = classifier.predict(X_valid)
 
     y_pred = (y_pred > 0.5)
-    cm = confusion_matrix(y_validation, y_pred)
+    cm = confusion_matrix(y_valid, y_pred)
     print(cm)
     diagonal_sum = cm.trace()
     sum_of_all_elements = cm.sum()
