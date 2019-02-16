34d0e1e3eeccca8cbe137ff9a3e95c8270d0c4a7
Narzhan <vasek.bruzek@gmail.com>
2019-02-16 13:25:56 +0100

    Baseline nn without embedding

--- training/word_embedding.py
+++ training/word_embedding.py
@@ -14,64 +14,6 @@ from sklearn.model_selection import train_test_split
 from keras.preprocessing.sequence import pad_sequences
 from nltk.stem import WordNetLemmatizer, SnowballStemmer
 nltk.download('wordnet')
-# feature_size = 100  # Word vector dimensionality
-# window_context = 20  # Context window size
-# min_word_count = 10  # Minimum word count
-# sample = 1e-3  # Downsample setting for frequent words
-#
-# # w2v_model = word2vec.Word2Vec(processed_docs, size=feature_size,
-# #                               window=window_context, min_count=min_word_count,
-# #                               sample=sample, iter=10)
-# fast_text = FastText(processed_docs, size=feature_size,
-#                               window=window_context, min_count=min_word_count,
-#                               sample=sample, iter=50)
-# fast_text.save("fast_text.pkl")
-# # w2v_model = word2vec.Word2Vec.load("gensim_we/w2v_model.pkl")
-#
-#
-#
-# # similar_words = {search_term: [item[0] for item in w2v_model.wv.most_similar([search_term], topn=5)]
-# #                   for search_term in ['malware', 'phishing', 'hoax', 'exploit', 'botnet', 'spam']}
-# # print(similar_words)
-#
-# def average_word_vectors(words, model, vocabulary, num_features):
-#     feature_vector = np.zeros((num_features,), dtype="float64")
-#     nwords = 0.
-#
-#     for word in words:
-#         if word in vocabulary:
-#             nwords = nwords + 1.
-#             feature_vector = np.add(feature_vector, model[word])
-#
-#     if nwords:
-#         feature_vector = np.divide(feature_vector, nwords)
-#
-#     return feature_vector
-#
-#
-# def averaged_word_vectorizer(corpus, model, num_features):
-#     vocabulary = set(model.wv.index2word)
-#     features = [average_word_vectors(tokenized_sentence, model, vocabulary, num_features)
-#                 for tokenized_sentence in corpus]
-#     return np.array(features)
-#
-#
-# w2v_feature_array = averaged_word_vectorizer(corpus=processed_docs, model=fast_text,
-#                                              num_features=feature_size)
-# # document_array = pandas.DataFrame(w2v_feature_array)
-# pickle.dump(w2v_feature_array, open("fasttext_feature_array.pkl", "wb"))
-# # w2v_feature_array = pickle.load(open("w2v_feature_array.pkl", "rb"))
-# # w2v_model = None
-# from sklearn.cluster import AffinityPropagation, MiniBatchKMeans
-#
-# # ap = AffinityPropagation()
-# ap = MiniBatchKMeans()
-# ap.fit(w2v_feature_array)
-# cluster_labels = ap.labels_
-# pickle.dump(cluster_labels, open("cluster_labels_fasttext.pkl", "wb"))
-# dataset.reset_index(drop=True, inplace=True)
-# cluster_labels = pandas.DataFrame(cluster_labels, columns=['ClusterLabel'])
-# print(pandas.concat([dataset, cluster_labels], axis=1))
 
 
 dataset = pandas.read_csv("text_test_data_splitted.csv", index_col=3, encoding='utf-8', delimiter=";", engine="python")
@@ -107,7 +49,7 @@ def preprocess(text):
 # seq_lengths = dataset.text.apply(lambda x: len(x.split(' ')))
 # print(seq_lengths.describe())
 
-processed_docs = customtrain_dataset['text'].map(preprocess)
+# processed_docs = customtrain_dataset['text'].map(preprocess)
 # processed_docs = pickle.load(open("splitted_docs.pkl", "rb"))
 # pickle.dump(processed_docs, open("splitted_text/word_embedding/splitted_docs.pkl", "wb"))
 feature_size = 300
@@ -117,45 +59,45 @@ sample = 1e-3
 
 
 ####### Fast Text
-fast_text = FastText(processed_docs, size=feature_size,
-                     window=window_context, min_count=min_word_count, sample=sample,
-                     iter=50)
-fast_text.save("splitted_text/word_embedding/fast_text/fast_text.pkl")
-fast_text.wv.save_word2vec_format("pretrained/custom_embedding.txt", binary=False)
-fast_text, processed_docs = None, None
+# fast_text = FastText(processed_docs, size=feature_size,
+#                      window=window_context, min_count=min_word_count, sample=sample,
+#                      iter=50)
+# fast_text.save("splitted_text/word_embedding/fast_text/fast_text.pkl")
+# fast_text.wv.save_word2vec_format("pretrained/custom_embedding.txt", binary=False)
+# fast_text, processed_docs = None, None
 
 ######Word2Vec
 # w2v_model = word2vec.Word2Vec(processed_docs, size=feature_size,
 #                               window=window_context, min_count=min_word_count,
 #                               sample=sample, iter=10)
 # w2v_model.save("w2v_model.pkl")
+w2v_model = word2vec.Word2Vec.load("splitted_text/word_embedding/w2v/w2v_custom_model.pkl")
 
-
-t = Tokenizer()
-t.fit_on_texts(dataset.text)
+# t = Tokenizer()
+# t.fit_on_texts(dataset.text)
+t= pickle.load(open("splitted_text/word_embedding/tokenizer.pkl","rb"))
 vocab_size = len(t.word_index) + 1
 embedding_matrix = np.zeros((vocab_size, 300))
-files_location = ["wiki.cs.vec", "wiki.de.vec", "wiki.en.vec", "wiki.es.vec", "wiki.fr.vec", "wiki.ja.vec",
-                  "wiki.ru.vec", "wiki.zh.vec"]
-# files_location = ["wiki.cs.vec"]
-for file_location in files_location:
-    with open("pretrained/{}".format(file_location), "r") as file:
-        next(file)
-        for line in file:
-            values = line.split()
-            if values[0] in t.word_index:
-                # word = values[0]
-                coefs = asarray(values[len(values)-300:], dtype='float32')
-                embedding_matrix[t.word_index[values[0]]] = coefs
-
-with open("pretrained/custom_embedding.txt", "r") as file:
-    for line in file:
-        values = line.split()
-        # if values[0] in t.word_index:
-        # word = values[0]
-        coefs = asarray(values[len(values) - 300:], dtype='float32')
-        embedding_matrix[t.word_index[values[0]]] = coefs
-np.save("splitted_text/word_embedding/fast_text/embedding_martix.npy", embedding_matrix)
+# files_location = ["wiki.cs.vec", "wiki.de.vec", "wiki.en.vec", "wiki.es.vec", "wiki.fr.vec", "wiki.ja.vec",
+#                   "wiki.ru.vec", "wiki.zh.vec"]
+# for file_location in files_location:
+#     with open("pretrained/{}".format(file_location), "r") as file:
+#         next(file)
+#         for line in file:
+#             values = line.split()
+#             if values[0] in t.word_index:
+#                 # word = values[0]
+#                 coefs = asarray(values[len(values)-300:], dtype='float32') # convert to float16
+#                 embedding_matrix[t.word_index[values[0]]] = coefs
+#
+# with open("pretrained/custom_embedding.txt", "r") as file:
+#     for line in file:
+#         values = line.split()
+#         # if values[0] in t.word_index:
+#         # word = values[0]
+#         coefs = asarray(values[len(values) - 300:], dtype='float32')
+#         embedding_matrix[t.word_index[values[0]]] = coefs
+# np.save("splitted_text/word_embedding/fast_text/embedding_martix.npy", embedding_matrix)
 
 ####Wod2vec
 # embedding_matrix = np.zeros((len(w2v_model.wv.vocab), feature_size))
@@ -165,6 +107,15 @@ np.save("splitted_text/word_embedding/fast_text/embedding_martix.npy", embedding
 #     embedding_vector = w2v_model.wv[w2v_model.wv.index2word[i]]
 #     if embedding_vector is not None:
 #         embedding_matrix[i] = embedding_vector
+for word in w2v_model.wv.vocab:
+    if word in t.word_index:
+        embedding_vector = w2v_model.wv[word]
+        if embedding_vector is not None:
+            embedding_matrix[t.word_index[word]] = embedding_vector
+np.save("splitted_text/word_embedding/w2v_embedding_martix_custom.npy", embedding_matrix)
+# from gensim.models import KeyedVectors
+# # Load vectors directly from the file
+# model = KeyedVectors.load_word2vec_format('data/GoogleGoogleNews-vectors-negative300.bin', binary=True)
 
 
 # X_train, X_test, y_train, y_test = train_test_split(dataset.text, dataset.label, test_size=0.1, random_state=7)
@@ -270,6 +221,33 @@ def create_bidirectional_rnn():
     eval_metric(history, "acc", "cz")
     eval_metric(history, "loss", "cz")
 
+def without_embedding():
+    # Add an Input Layer
+    input_layer = Input((MAX_LEN,))
+
+    # Add the word embedding Layer
+    embedding_layer = Embedding(vocab_size, feature_size)(input_layer)
+    embedding_layer = SpatialDropout1D(0.3)(embedding_layer)
+
+    # Add the LSTM Layer
+    lstm_layer = LSTM(num_neurons)(embedding_layer)
+
+    # Add the output Layers
+    output_layer1 = Dense(50, activation="relu")(lstm_layer)
+    output_layer1 = Dropout(0.25)(output_layer1)
+    output_layer2 = Dense(1, activation="sigmoid")(output_layer1)
+
+    # Compile the model
+    model = Model(inputs=input_layer, outputs=output_layer2)
+    model.compile(optimizer="adam", loss='binary_crossentropy', metrics=['accuracy'])
+
+    # Fitting our model
+    history = model.fit(X_train, y_train, batch_size=batch, nb_epoch=num_epoches)
+    evaluate_model(model)
+    eval_metric(history, "acc", "en")
+    eval_metric(history, "loss", "en")
+    eval_metric(history, "acc", "cz")
+    eval_metric(history, "loss", "cz")
 
 def eval_metric(history, metric_name, lang):
     '''
@@ -318,3 +296,63 @@ def evaluate_model(classifier):
 # model_glove.add(LSTM(100))
 # model_glove.add(Dense(1, activation='sigmoid'))
 # model_glove.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+# feature_size = 100  # Word vector dimensionality
+# window_context = 20  # Context window size
+# min_word_count = 10  # Minimum word count
+# sample = 1e-3  # Downsample setting for frequent words
+#
+# # w2v_model = word2vec.Word2Vec(processed_docs, size=feature_size,
+# #                               window=window_context, min_count=min_word_count,
+# #                               sample=sample, iter=10)
+# fast_text = FastText(processed_docs, size=feature_size,
+#                               window=window_context, min_count=min_word_count,
+#                               sample=sample, iter=50)
+# fast_text.save("fast_text.pkl")
+# # w2v_model = word2vec.Word2Vec.load("gensim_we/w2v_model.pkl")
+#
+#
+#
+# # similar_words = {search_term: [item[0] for item in w2v_model.wv.most_similar([search_term], topn=5)]
+# #                   for search_term in ['malware', 'phishing', 'hoax', 'exploit', 'botnet', 'spam']}
+# # print(similar_words)
+#
+# def average_word_vectors(words, model, vocabulary, num_features):
+#     feature_vector = np.zeros((num_features,), dtype="float64")
+#     nwords = 0.
+#
+#     for word in words:
+#         if word in vocabulary:
+#             nwords = nwords + 1.
+#             feature_vector = np.add(feature_vector, model[word])
+#
+#     if nwords:
+#         feature_vector = np.divide(feature_vector, nwords)
+#
+#     return feature_vector
+#
+#
+# def averaged_word_vectorizer(corpus, model, num_features):
+#     vocabulary = set(model.wv.index2word)
+#     features = [average_word_vectors(tokenized_sentence, model, vocabulary, num_features)
+#                 for tokenized_sentence in corpus]
+#     return np.array(features)
+#
+#
+# w2v_feature_array = averaged_word_vectorizer(corpus=processed_docs, model=fast_text,
+#                                              num_features=feature_size)
+# # document_array = pandas.DataFrame(w2v_feature_array)
+# pickle.dump(w2v_feature_array, open("fasttext_feature_array.pkl", "wb"))
+# # w2v_feature_array = pickle.load(open("w2v_feature_array.pkl", "rb"))
+# # w2v_model = None
+# from sklearn.cluster import AffinityPropagation, MiniBatchKMeans
+#
+# # ap = AffinityPropagation()
+# ap = MiniBatchKMeans()
+# ap.fit(w2v_feature_array)
+# cluster_labels = ap.labels_
+# pickle.dump(cluster_labels, open("cluster_labels_fasttext.pkl", "wb"))
+# dataset.reset_index(drop=True, inplace=True)
+# cluster_labels = pandas.DataFrame(cluster_labels, columns=['ClusterLabel'])
+# print(pandas.concat([dataset, cluster_labels], axis=1))
