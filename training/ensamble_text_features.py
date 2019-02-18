2192f791624d46a7c30d45fb77518ee9b145edc7
Narzhan <vasek.bruzek@gmail.com>
2019-02-18 11:23:45 +0100

    Lightgbm fixed

--- training/ensamble_text_features.py
+++ training/ensamble_text_features.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import numpy as np
 import pickle
-from catboost import CatBoostClassifier
-from sklearn import model_selection
-from sklearn.ensemble import BaggingClassifier, RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
-from xgboost import XGBClassifier
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-import gc
-
+#from catboost import CatBoostClassifier
+#from sklearn import model_selection
+#from sklearn.ensemble import BaggingClassifier, RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+#from xgboost import XGBClassifier
+#import lightgbm as lgb
 # array = dataset.values
 # X = array[:, 0:10]
 # Y = array[:, 10]
@@ -24,8 +22,8 @@ def load_data(file: str):
                               engine="python")
     dataset = dataset.replace(np.nan, '', regex=True)
     dataset = dataset.sort_index()
-    # feature_array = pickle.load(open(file, "rb"))
-    feature_array = np.load(file)
+    feature_array = pickle.load(open(file, "rb"))
+    #feature_array = np.load(file)
     mapper = {}
     domain_mapping = {}
     row_id = 0
@@ -42,19 +40,23 @@ def load_data(file: str):
     for domain, id_list in mapper.items():
         temp_list = []
         for ids in id_list:
-            # temp_list.extend(feature_array[ids].toarray()[0])
-            temp_list.extend(feature_array[ids])
+            temp_list.extend(feature_array[ids].toarray()[0])
+            #temp_list.extend(feature_array[ids])
         domain_mapping[domain]["features"] = tuple(temp_list)
     print("features done")
     feature_array, mapper = None, None
+    #pickle.dump(domain_mapping, open("domain_mapping.pkl", "wb"))
     #final_dataset = pd.DataFrame(list(domain_mapping.values()), index=domain_mapping.keys())
     #tmp = final_dataset.features.apply(pd.Series)
     #final_dataset.drop(["features"], axis=1)
     #final_dataset = tmp.merge(final_dataset, right_index=True, left_index=True)
     #final_dataset = final_dataset.features.apply(pd.Series).merge(final_dataset, right_index=True, left_index=True).drop(["features"], axis=1)
     temp_dataset = pd.DataFrame(list(domain_mapping.values()), index=list(domain_mapping.keys()))
-    final_dataset = pd.DataFrame(temp_dataset['features'].values.tolist(), index=list(domain_mapping.keys()))
+    domain_mapping = None
+    temp_dataset.to_csv("tf_idf/temp_dataset.csv")
+    final_dataset = pd.DataFrame(temp_dataset['features'].values.tolist(), index=temp_dataset.index.tolist())
     final_dataset["label"] = temp_dataset["label"]
+    temp_dataset = None
     final_dataset = final_dataset.replace(np.nan, 0, regex=True)
     print("final done")
     return final_dataset
@@ -62,19 +64,19 @@ def load_data(file: str):
 def models():
     print("Test started")
     models = []
-    models.append(('BMeta', BaggingClassifier()))
-    models.append(('RForest', RandomForestClassifier(n_estimators=100)))
-    models.append(('Ada', AdaBoostClassifier()))
-    models.append(('GBC', GradientBoostingClassifier()))
+#    models.append(('BMeta', BaggingClassifier()))
+#    models.append(('RForest', RandomForestClassifier(n_estimators=100)))
+#    models.append(('Ada', AdaBoostClassifier()))
+#    models.append(('GBC', GradientBoostingClassifier()))
     models.append(('XGB', XGBClassifier()))
-    models.append(('CatBoost', CatBoostClassifier(iterations=2, learning_rate=1, depth=2, loss_function='Logloss')))
+#    models.append(('CatBoost', CatBoostClassifier(iterations=2, learning_rate=1, depth=2, loss_function='Logloss')))
     scoring = "accuracy"
     results = []
     names = []
     for name, model in models:
         try:
             kfold = model_selection.KFold(n_splits=10, random_state=seed)
-            cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring, n_jobs=-1)
+            cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring, n_jobs=3)
             results.append(cv_results)
             names.append(name)
             msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
@@ -83,41 +85,54 @@ def models():
         else:
             print(msg)
 
+def light_gbm():
+        # train_data = lgb.Dataset(X_train, label=Y_train)
+    lgb_train = lgb.Dataset(X_train, Y_train)
+    lgb_eval = lgb.Dataset(X_validation, Y_validation, reference=lgb_train)
+        # param = {'num_leaves': 31, 'num_trees': 50, 'objective': 'binary', 'metric': 'auc'}
+    param = {
+    'task': 'train',
+    'boosting_type': 'gbdt',
+    'objective': 'binary',
+    'metric': {'l2', 'auc', "binary_logloss"},
+    'num_leaves': 31,
+    'learning_rate': 0.05,
+    'feature_fraction': 0.9,
+    'bagging_fraction': 0.8,
+    'bagging_freq': 5,
+    'verbose': 0
+        }
+    bst = lgb.train(param, lgb_train, 20, valid_sets=lgb_eval)
+    y_pred = bst.predict(X_validation, num_iteration=bst.best_iteration)
+    for i in range(0, Y_validation.shape[0]):
+       if y_pred[i]>=.5:       # setting threshold to .5
+           y_pred[i]=1
+       else:  
+           y_pred[i]=0
+    from sklearn.metrics import confusion_matrix
+    cm = confusion_matrix(Y_validation, y_pred)
+    print(cm)
+    from sklearn.metrics import accuracy_score
+    accuracy=accuracy_score(y_pred,Y_validation)
+    print("LightGBM: {}".format(accuracy))
+    #print('The mean of prediction is:', y_pred.mean(), y_pred.std())
 
-def train_model(name: str):
-    classifier = RandomForestClassifier(n_estimators=100, n_jobs=-1)
-    classifier.fit(X_train, Y_train)
-    X_predicted = classifier.predict(X_train)
-    predictions = classifier.predict(X_validation)
-    print(accuracy_score(Y_validation, predictions))
-    print(classification_report(Y_validation, predictions))
-    result_dataset = pd.DataFrame(data=np.concatenate((X_predicted, predictions), axis=0),
-                                  index=np.concatenate((domains_train, domains_test), axis=0), columns=[name])
-    result_dataset.to_csv("splitted_text/tf_idf/result_data.csv")
-    pickle.dump(classifier, open("splitted_text/tf_idf/model.pkl", "wb"))
-
-
-for i in [2]:
+for i in [1]:
     print("Going over {}% min df".format(i))
     print("#######################################")
-    # dataset = load_data("splitted_text/tf_idf/topics_{}.npy".format(i))
-    # dataset.to_csv("splitted_text/tf_idf/dataframe_{}.csv".format(i))
-    dataset = pd.read_csv("splitted_text/tf_idf/dataframe_{}.csv".format(i), index_col=0)
+    dataset = load_data("tf_idf/features_{}.pkl".format(i))
+    dataset.to_csv("tf_idf/dataframe_{}.csv".format(i))
+    #dataset = pd.read_csv("tf_idf/dataframe_{}.csv".format(i), index_col=0, encoding='utf-8')
     array = dataset.values
-    domains = dataset.index.values
-    dataset = None
+    dataset=None
     X = array[:, 0:-1]
     Y = array[:, -1]
-
     print("array created")
     array = None
     validation_size = 0.20
     seed = 7
-    X_train, X_validation, Y_train, Y_validation, domains_train, domains_test = model_selection.train_test_split(X, Y,
-                                                                                                                 domains,
-                                                                                                                 test_size=validation_size,
-                                                                                                                 random_state=seed)
-    domains = None
+    X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X,Y, test_size=validation_size,
+                                                                                                                   random_state=seed)
     X, Y = None, None
-    train_model("topics")
-    # models()
+    light_gbm()
+    models()
