c4eec20599dbc4a4cf7362f5d0384176cfc51ab3
Narzhan <vasek.bruzek@gmail.com>
2019-10-14 08:57:03 +0200

    Fixed excessive logging from workers

--- domain_analyzer/analyzer/tools.py
+++ domain_analyzer/analyzer/tools.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from logging.handlers import RotatingFileHandler
 
 
 def build_logger(name: str, log_path: str, log_level: str = "INFO"):
@@ -10,16 +11,17 @@ def build_logger(name: str, log_path: str, log_level: str = "INFO"):
 
     logger = logging.getLogger(name)
     logger.setLevel(log_level)
+    if not logger.handlers:
+        handler = RotatingFileHandler("{}/{}.log".format(log_path, name), maxBytes=20000000, backupCount=12, encoding="utf-8")
+        # handler = logging.FileHandler("{}/{}.log".format(log_path, name), encoding="utf-8")
+        handler.setLevel(log_level)
 
-    handler = logging.FileHandler("{}/{}.log".format(log_path, name), encoding="utf-8")
-    handler.setLevel(log_level)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(log_level)
 
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(log_level)
-
-    formatter = logging.Formatter('%(asctime)s - %(lineno)d - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.addHandler(console_handler)
+        formatter = logging.Formatter('%(asctime)s - %(lineno)d - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.addHandler(console_handler)
 
     return logger
