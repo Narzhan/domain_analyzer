4e155a810ae45125f4a07905020dca5b53d56247
Narzhan <vasek.bruzek@gmail.com>
2019-10-14 09:03:45 +0200

    Gunicorn sync worker replaced with gevent

--- domain_analyzer/startup.sh
+++ domain_analyzer/startup.sh
@@ -3,5 +3,5 @@ if [ $MODE = "domain_analyzer" ]
 then
     celery -A main worker --autoscale=6,1 --loglevel=info
 else
-    gunicorn -b 0.0.0.0:8000 --access-logfile /opt/domain_analyzer/logs/api_access.log app:app
+    gunicorn -b 0.0.0.0:8000 --worker-class gevent --access-logfile "-" app:app
 fi
